75073712715c71e01063a4769c69f939783daa79
Himanshu Gupta <thinktank.himanshu@gmail.com>
2021-07-08 17:53:11 +0530

    new example try 6

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,15 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="stylesheet" href="./styles/video.css" />
-          <script src="//cdn.8thwall.com/web/aframe/8frame-1.1.0.min.js"></script>
+          <script
+            async
+            src="//cdn.8thwall.com/web/aframe/8frame-1.1.0.min.js"
+          ></script>
 
-          <script src="//cdn.8thwall.com/web/xrextras/xrextras.js"></script>
+          <script
+            async
+            src="//cdn.8thwall.com/web/xrextras/xrextras.js"
+          ></script>
 
           <script
             async
