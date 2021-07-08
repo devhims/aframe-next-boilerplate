7193d832e6c2033ac3b6b379bcdd9afe1477fc16
Himanshu Gupta <thinktank.himanshu@gmail.com>
2021-07-08 17:50:44 +0530

    new example try 5

--- components/BlankAframe.js
+++ components/BlankAframe.js
@@ -20,29 +20,6 @@ const BlankAframe = () => {
     <>
       <Head>
         <title>Blank Aframe</title>
-        <Script
-          src="//cdn.8thwall.com/web/aframe/8frame-1.1.0.min.js"
-          strategy="beforeInteractive"
-        />
-        <Script
-          src="//cdn.8thwall.com/web/xrextras/xrextras.js"
-          strategy="beforeInteractive"
-        />
-
-        <Script
-          async
-          src="//apps.8thwall.com/xrweb?appKey=mSofXbWTdanwMZsJkSv5HKia091PsOzQa2IIMudbjs7FW5uMBkUGD2gQfVKSnyISXunV1Q"
-          strategy="beforeInteractive"
-        />
-
-        {/* <script src="//cdn.8thwall.com/web/aframe/8frame-1.1.0.min.js"></script>
-
-        <script src="//cdn.8thwall.com/web/xrextras/xrextras.js"></script>
-
-        <script
-          async
-          src="//apps.8thwall.com/xrweb?appKey=mSofXbWTdanwMZsJkSv5HKia091PsOzQa2IIMudbjs7FW5uMBkUGD2gQfVKSnyISXunV1Q"
-        ></script> */}
       </Head>
       {xrReady && (
         <Scene
