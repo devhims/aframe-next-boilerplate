ba18da819317226229cf83fc12c0712e09241c5f
Himanshu Gupta <thinktank.himanshu@gmail.com>
2021-07-09 22:23:25 +0530

    css width and height update of aframe div 13

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import BlankAframe from '../components/BlankAframe';
+
+const Home = () => {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <BlankAframe />
+    </div>
+  );
+};
+
+export default Home;
