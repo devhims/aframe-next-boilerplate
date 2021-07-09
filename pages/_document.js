98ad75242a5520577464d3f07bce9acd512ef5dd
Himanshu Gupta <thinktank.himanshu@gmail.com>
2021-07-09 22:06:49 +0530

    css width and height update of aframe div 9

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <script src="//cdn.8thwall.com/web/aframe/8frame-1.1.0.min.js"></script>
-
           <script src="//cdn.8thwall.com/web/xrextras/xrextras.js"></script>
-
           <script
             async
             src="//apps.8thwall.com/xrweb?appKey=mSofXbWTdanwMZsJkSv5HKia091PsOzQa2IIMudbjs7FW5uMBkUGD2gQfVKSnyISXunV1Q"
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 import BlankAframe from '../components/BlankAframe';
 
 // const CubeDemo = dynamic(
